d42ed7c0210c7e1b58536cdec1249ded8b255e4b
ucsp09 <51616719+ucsp09@users.noreply.github.com>
2019-12-20 19:27:13 +0530

    Main file

---
+++ Graph.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<map>
+#include<vector>
+using namespace std;
+template <typename T>
+class graph
+{
+  private: map< T , vector<T> > g;
+}
+int main()
+{
+  return 0;
+}
