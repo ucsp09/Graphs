deb4ae13e8ac31afb2a79b21878dd2907ffb6632
ucsp09 <51616719+ucsp09@users.noreply.github.com>
2019-12-20 19:48:19 +0530

    pushed void add_edge(T node1,T node2) function

--- Graph.cpp
+++ Graph.cpp
@@ -19,6 +19,19 @@ class graph
 					throw "This node already Exists";
 				}
 			}
+			void add_edge(T node1,T node2)
+			{
+				if(node1==node2)
+					throw "Self edges are not allowed";
+				else
+				{
+					if(g.find(node1)!=g.end())
+						if(find(g[node1].begin(),g[node1].end(),node2)!=g[node1].end())
+							throw " This Edge already Exists";							
+					g[node1].push_back(node2);
+					g[node2].push_back(node1);			
+				}
+			}
 }
 int main()
 {
@@ -26,8 +39,11 @@ int main()
 	graph<int> g1;
 	g1.add_node(3);
 	g1.add_node(4);
-  	g1.add_node(4);
  	g1.add_node(5);
+		g1.add_edge(2,3);
+		g1.add_edge(5,6);
+		g1.add_edge(5,6);
+		h1.add_edge(2,3);
 	}
 	catch(const char* msg){
 		cout<<endl<<msg;
