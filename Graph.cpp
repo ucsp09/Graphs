d58a7b070d8d5d4b14ce9a4c811e14e0ba550473
ucsp09 <51616719+ucsp09@users.noreply.github.com>
2019-12-21 15:11:29 +0530

    Created graph interface

--- Graph.cpp
+++ Graph.cpp
@@ -7,8 +7,18 @@ using namespace std;
 template <typename T>
 class graph_interface{
 	public:	map< T , vector<T> > g;
-			virtual void add_node(T a)=0;
 			virtual void add_edge(T node1,T node2)=0;
+			void add_node(T a)//complexity O(logn) to find if node exists and then O(1) to add node
+			{
+				if(g.find(a)==g.end())//find() std::map function
+				{
+					vector<T> vec;
+					g[a]=vec;
+					cout<<endl<<"node"<<" "<<a<<" "<<"added successfully";
+				}
+				else
+					throw "This node already exists(cannot add again)";
+			}
 			void display()//complexity O(nodes*edges)
 			{
 				typename map< T, vector<T> >::iterator it;
@@ -22,18 +32,7 @@ class graph_interface{
 };
 template <typename T>
 class undirected_graph:public graph_interface<T>{
-	public:	void add_node(T a)//complexity O(logn) to find if node exists and then O(1) to add node
-			{
-				if(graph_interface<T>::g.find(a)==graph_interface<T>::g.end())//find() std::map function
-				{
-					vector<T> vec;
-					graph_interface<T>::g[a]=vec;
-					cout<<endl<<"node"<<" "<<a<<" "<<"added successfully";
-				}
-				else
-					throw "This node already exists(cannot add again)";
-			}
-			void add_edge(T node1,T node2)//complexity O(n) to find if edge exists and then O(1) to add edge
+	public:	void add_edge(T node1,T node2)//complexity O(n) to find if edge exists and then O(1) to add edge
 			{
 				if(node1==node2)
 					throw "Self edges are not allowed";
